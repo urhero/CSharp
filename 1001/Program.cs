209a5ff46192663deaf501333395e09b5828c17e
urhero <inkyu.will.moon@gmail.com>
2016-04-01 14:22:04 +0900

    20160401_1421

---
+++ 1001/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class P
+{
+    static void Main()
+    {
+        string[] l = Console.ReadLine().Split(' ');
+        int n = Convert.ToInt32(l[0]);
+        for (int i = n; i > 0; i--)
+            Console.WriteLine(i);
+    }
+}
